2e7b600685d7ed522e6dbb308ddfced4b864a7eb
Leonardo <leonardostudent94@hotmail.com>
2019-03-01 00:22:31 -0300

    modeling of entities

--- src/backoffice/model/customer.model.ts
+++
@@ -1,9 +0,0 @@
-export class Customer {
-  constructor(
-    public name: string,
-    public document: string,
-    public email: string,
-    public password: string,
-    public active: boolean,
-  ) {}
-}
